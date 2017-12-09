c23b9eaf47d1d4fd26be7fd8f137a629194132b1
ialami <alami.ismail_1995@yahoo.fr>
2017-12-09 18:30:54 +0000

    add friend done

--- My_project/starter-code/controllers/requests.js
+++ My_project/starter-code/controllers/requests.js
@@ -36,55 +36,138 @@ function getRequests(req, res, next){
     .catch(next);
 }
 
+function acceptFriend(req, res){
+  return new Promise((resolve, reject) => {
+    Request
+      .findByIdAndUpdate(req.params.id, req.body)
+      .exec()
+      .then(friendRequest => {
+        friendRequest = Object.assign(friendRequest, req.body);
+        return friendRequest.save();
+      })
+      .then(friendRequest => {
+        // res.json(friendRequest);
+        resolve(friendRequest);
+      })
+      .catch(reject);
+  });
+}
+
 function acceptRequest(req, res, next){
-  Request
-    .findByIdAndUpdate(req.params.id, req.body)
-    .exec()
-    .then(acceptedRequest => {
-      acceptedRequest = Object.assign(acceptedRequest, req.body);
-      // acceptedRequest = Object.assign(acceptedRequest, { status: 'accepted' });
-      return acceptedRequest.save(), acceptedRequest;
-    })
-    .then(acceptedRequest => {
-      User
-        .findById({ _id: acceptedRequest.receiver })
-        // .findById({$and: [ // ERR: can't set headers after they are sent
-        //   { _id: acceptedRequest.receiver },
-        //   { _id: acceptedRequest.sender }
-        // ]})
-        .exec()
-        .then(users => {
-// console.log('users in promise', users);
-          // return users;
+  return acceptFriend(req, res)
+    .then(friendRequest => {
+      // console.log('friendRequest', friendRequest);
+      const sender = friendRequest.sender;
+      const receiver = friendRequest.receiver;
+
+      getSender(sender)
+        .then(sender => {
+          getReceiver(receiver)
+            .then(receiver => {
+              // console.log('friendRequest', friendRequest);
+              // console.log('this is the sender:', sender);
+              // console.log('this is the receiver before pushing:', receiver);
+              receiver.friends.push(sender);
+              // console.log('this is the receiver after pushing:', receiver);
+              return res.json([friendRequest, sender, receiver]);
+            })
+            .catch(next);
         })
         .catch(next);
 
-// console.log('Currentuser outside promise', Currentuser);
-// console.log('acceptedRequest', acceptedRequest);
+      getReceiver(receiver)
+        .then(receiver => {
+          getSender(sender)
+            .then(sender => {
+              console.log('friendRequest', friendRequest);
+              console.log('this is the receiver:', receiver);
+              console.log('this is the sender before pushing:', sender);
+              sender.friends.push(receiver);
+              console.log('this is the sender after pushing:', sender);
+            })
+            .catch(next);
+        })
+        .catch(next);
+
+
     })
-    .then(acceptedRequest => res.json(acceptedRequest))
     .catch(next);
 }
-
+//
 // function acceptRequest(req, res, next){
-//   GetCurrentUser(acceptedRequest)
-//     .()
-//
+//   Request
+//     .findByIdAndUpdate(req.params.id, req.body)
+//     .exec()
+//     .then(acceptedRequest => {
+//       acceptedRequest = Object.assign(acceptedRequest, req.body);
+//       // acceptedRequest = Object.assign(acceptedRequest, { status: 'accepted' });
+//       return [acceptedRequest.save(), acceptedRequest, 'hey man'];
+//     })
+//     .then((res) => {
+// // console.log('req.body', req.body);
+// // console.log('res', res);
+// console.log('res[1]', res[1]);
+// // console.log('res[1].receiver', res[1].receiver);
+// // console.log('res[1].sender', res[1].sender);
+//       const receiverId = res[1].receiver;
+//       // const currentUser = () => {
+//       //   return GetCurrentUser(receiverId);
+//       // };
+//       // const currentUser = GetCurrentUser(receiverId);
+//       const currentUser = GetCurrentUser.resolve(receiverId);
+// //         .then(currentUser => {
+// // console.log('response from .then', currentUser);
+// //           return currentUser;
+// //         });
+// console.log('currentUser in controller', currentUser);
+//       return currentUser;
+//     })
+// //     .then(acceptedRequest => {
+// //       User
+// //         .findById({ _id: acceptedRequest.receiver })
+// //         // .findById({$and: [ // ERR: can't set headers after they are sent
+// //         //   { _id: acceptedRequest.receiver },
+// //         //   { _id: acceptedRequest.sender }
+// //         // ]})
+// //         .exec()
+// //         .then(users => {
+// // // console.log('users in promise', users);
+// //           // return users;
+// //         })
+// //         .catch(next);
+// //
+// // // console.log('Currentuser outside promise', Currentuser);
+// // // console.log('acceptedRequest', acceptedRequest);
+// //     })
+//     .then(acceptedRequest => res.json(acceptedRequest))
+//     .catch(next);
 // }
-//
-// const GetCurrentUser = (acceptedRequest) => {
-//   return new Promise((resolve, reject) => {
-// console.log('fired');
-//     User
-//       .findById({ _id: acceptedRequest.receiver })
-//       .exec()
-//       .then(Currentuser => {
+
+const getReceiver = (receiverId) => {
+  return new Promise((resolve, reject) => {
+    User
+      .findById({ _id: receiverId })
+      .exec()
+      .then(Currentuser => {
 // console.log('Currentuser in promise', Currentuser);
-//         return resolve(Currentuser);
-//       })
-//       .catch(reject);
-//   });
-// };
+        return resolve(Currentuser);
+      })
+      .catch(reject);
+  });
+};
+
+const getSender = (senderId) => {
+  return new Promise((resolve, reject) => {
+    User
+      .findById({ _id: senderId })
+      .exec()
+      .then(sender => {
+// console.log('sender in getSender', sender);
+        return resolve(sender);
+      })
+      .catch(reject);
+  });
+};
 
 module.exports = {
   addFriend: addFriend,
